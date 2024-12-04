9b1d7dcf5cf93ad0d4ddd8329ef7edec2166edec
Joeyluv03 <90421255+Joeyluv03@users.noreply.github.com>
2024-12-04 06:40:50 -0800

    new

--- personal-site-p5/js/sketch.js
+++ personal-site-p5/js/sketch.js
@@ -1,32 +1,32 @@
 
-// let canvas;
-// let xPos = 0;
-// let yPos = 0;
-// let easing = 0.05;
+let canvas;
+let xPos = 0;
+let yPos = 0;
+let easing = 0.05;
 
-// function setup(){
-//     canvas = createCanvas(windowWidth, windowHeight);
-//     canvas.position(0,0);
-//     canvas.style("z-index",-2);
-//     //background(225);
-// }
+function setup(){
+    canvas = createCanvas(windowWidth, windowHeight);
+    canvas.position(0,0);
+    canvas.style("z-index",-2);
+    //background(225);
+}
 
-// function windowResized(){
-//     resizeCanvas(windowWidth, windowHeight);
-// }
+function windowResized(){
+    resizeCanvas(windowWidth, windowHeight);
+}
 
-// function draw(){
-//     clear();
+function draw(){
+    clear();
 
-//     xPos = xPos + ((mouseX - xPos) * easing);
-//     yPos = yPos + ((mouseY - yPos) * easing);
+    xPos = xPos + ((mouseX - xPos) * easing);
+    yPos = yPos + ((mouseY - yPos) * easing);
     
-//     drawThing(xPos, yPos);
+    drawThing(xPos, yPos);
 
-// }
+}
 
-// function drawThing(_x,_y){
-//     noStroke();
-//     fill(random(200,255),random(200,255),random(200,255),);
-//     ellipse(_x, _y, 30, 30);
-// }
+function drawThing(_x,_y){
+    noStroke();
+    fill(random(200,255),random(200,255),random(200,255),);
+    ellipse(_x, _y, 30, 30);
+}
